7c0ade5eb9cc88c767e9e0a39e4b301cda40ff4b
Billioma <ibeomari@gmail.com>
2023-07-03 06:52:10 +0100

    .

--- src/components/layout/PageLayout.js
+++ src/components/layout/PageLayout.js
@@ -4,11 +4,10 @@ import Footer from "./Footer";
 
 const PageLayout = ({ children }) => {
   return (
-    <div className="font-[Sailec] lg:overflow-auto sm:overflow-hidden w-full flex flex-col min-h-screen">
+    <div className="font-[Sailec] overflow-hidden w-full flex flex-col min-h-screen">
       <Header />
       <div className="flex justify-center items-center">
         <div className="w-full lg:w-[1296px] min-h-screen px-[20px]">
-          {" "}
           {children}
         </div>
       </div>
