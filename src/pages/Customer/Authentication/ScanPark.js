63f0134fd3994b9e4c6730c552056a93308e3c6c
Billioma <ibeomari@gmail.com>
2023-11-20 13:35:54 +0100

    updates

--- src/pages/Customer/Authentication/ScanPark.js
+++ src/pages/Customer/Authentication/ScanPark.js
@@ -12,8 +12,8 @@ const ScanPark = () => {
       {isMobile ? (
         <>
           <BarcodeScannerComponent
-            width="100vw"
-            height="100vh"
+        width={500}
+        height={500}
             onUpdate={(err, result) => {
               if (result) setData(result.text);
               else setData("Not Found");
