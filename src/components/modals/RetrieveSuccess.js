aab2fd9b01693f71bb35a62aa9bb44fe1c057898
Billioma <ibeomari@gmail.com>
2025-09-25 14:10:14 +0100

    fe-fix

--- src/components/modals/RetrieveSuccess.js
+++ src/components/modals/RetrieveSuccess.js
@@ -29,7 +29,7 @@ const RetrieveSuccess = ({ isOpen, onClose, isRetrieve }) => {
         </Text>
 
         <Text textAlign="center" mt="10px">
-          Your Vehicle has successfully been retrieved
+          {isRetrieve ? "" : "Your Vehicle has successfully been retrieved"}
         </Text>
 
         <Flex mt="32px" w="full" display={isRetrieve ? "none" : "flex"}>
